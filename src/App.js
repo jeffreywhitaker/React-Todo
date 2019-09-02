c86daeb1bd8dcbfdec91f3959e3d9eb2ec92ee66
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-09-02 13:26:05 -0700

    began work on todo, setting up app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React from 'react'
+import TodoList from './components/TodoComponents/TodoList'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      message: ''
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,9 +15,10 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
